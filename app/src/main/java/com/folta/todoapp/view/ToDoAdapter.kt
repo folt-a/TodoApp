7bf4c3b1d6001bf5d60864bb3db12c2fdb11c508
Folta <faultun@gmail.com>
2019-08-28 23:41:51 +0900

    viewholder this削除

--- app/src/main/java/com/folta/todoapp/view/ToDoAdapter.kt
+++ app/src/main/java/com/folta/todoapp/view/ToDoAdapter.kt
@@ -68,16 +68,16 @@ open class ToDoAdapter(var items: List<ToDo>) : RecyclerView.Adapter<ToDoAdapter
 
         init {
             itemView.title.setOnClickListener { v ->
-                this.title.isFocusable = true
-                this.title.isFocusableInTouchMode = true
-                this.title.requestFocus()
+                title.isFocusable = true
+                title.isFocusableInTouchMode = true
+                title.requestFocus()
                 inputMethodManager?.showSoftInput(v, 1)
             }
             itemView.title.setOnEditorActionListener { v, actionId, _ ->
                 when (actionId) {
                     EditorInfo.IME_ACTION_DONE -> {
-                        this.title.isFocusable = false
-                        this.title.isFocusableInTouchMode = false
+                        title.isFocusable = false
+                        title.isFocusableInTouchMode = false
                         inputMethodManager?.hideSoftInputFromWindow(v.windowToken, 0)
                         true
                     }
@@ -93,38 +93,38 @@ open class ToDoAdapter(var items: List<ToDo>) : RecyclerView.Adapter<ToDoAdapter
 
             itemView.detail.setOnFocusChangeListener { v, hasFocus ->
                 if (hasFocus) return@setOnFocusChangeListener
-                this.content.isEnabled = false
-                this.content.isFocusable = false
-                this.content.isFocusableInTouchMode = false
-                this.content.background = null
+                content.isEnabled = false
+                content.isFocusable = false
+                content.isFocusableInTouchMode = false
+                content.background = null
                 inputMethodManager?.hideSoftInputFromWindow(v.windowToken, 0)
             }
             itemView.detail.setOnClickListener { v ->
                 Logger.d(isShowDetail.toString())
                 if (isShowDetail) {
-                    adapter.onFocusOut(this, this.adapterPosition)
-                    this.content.background = null
-                    this.content.setPadding(
+                    adapter.onFocusOut(this, adapterPosition)
+                    content.background = null
+                    content.setPadding(
                         0,
                         0,
                         0,
                         0
                     )
-                    this.content.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f)
-                    val mlp = this.content.layoutParams
+                    content.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f)
+                    val mlp = content.layoutParams
                     if (mlp is ViewGroup.MarginLayoutParams) mlp.setMargins(0)
                     detail.setImageResource(R.drawable.ic_detail)
                 } else {
-                    adapter.onDetailClick(this, this.adapterPosition)
-                    this.content.setBackgroundResource(R.drawable.edittext_content)
-                    this.content.setPadding(
+                    adapter.onDetailClick(this, adapterPosition)
+                    content.setBackgroundResource(R.drawable.edittext_content)
+                    content.setPadding(
                         v.context.resources.getDimensionPixelSize(R.dimen.dp8),
                         v.context.resources.getDimensionPixelSize(R.dimen.dp8),
                         v.context.resources.getDimensionPixelSize(R.dimen.dp8),
                         v.context.resources.getDimensionPixelSize(R.dimen.dp8)
                     )
-                    this.content.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
-                    val mlp = this.content.layoutParams
+                    content.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
+                    val mlp = content.layoutParams
                     if (mlp is ViewGroup.MarginLayoutParams) {
                         mlp.setMargins(
                             0,
@@ -137,9 +137,9 @@ open class ToDoAdapter(var items: List<ToDo>) : RecyclerView.Adapter<ToDoAdapter
                     detail.setImageResource(R.drawable.ic_detail_selected)
                 }
 
-                this.content.isEnabled = !this.content.isEnabled
-                this.content.isFocusable = !this.content.isFocusable
-                this.content.isFocusableInTouchMode = !this.content.isFocusableInTouchMode
+                content.isEnabled = !content.isEnabled
+                content.isFocusable = !content.isFocusable
+                content.isFocusableInTouchMode = !content.isFocusableInTouchMode
 
                 inputMethodManager?.hideSoftInputFromWindow(v.windowToken, 0)
                 isShowDetail = !isShowDetail
